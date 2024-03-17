56f6b8cf893f6233a7d7554ce1509b3acd9a786e
Volodymyr Podkorytov <131005653+VolodymyrPodkor@users.noreply.github.com>
2024-03-17 02:46:51 +0200

    Update BotConstants.java
    смена токена

--- src/main/java/org/example/feature/telegram/BotConstants.java
+++ src/main/java/org/example/feature/telegram/BotConstants.java
@@ -3,6 +3,7 @@ package org.example.feature.telegram;
 public class BotConstants {
 
     // ВАШ ТЕЛЕГРАМ БОТ
-    public static final String BOT_NAME = "CurrencyBOT";
-    public static final String BOT_TOKEN = "6588408839:AAHitwT7jLfI6M4bSqMHDB5rUUtyaQ0pn74";
+    public static final String BOT_NAME = "11111";
+
+    public static final String BOT_TOKEN = "11111";
 }
